a789edc0cffb1ff580abdffc30773b75dfef9296
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2023-09-14 13:58:47 +0800

    add some algorithm

--- sb_dqn_trade.py
+++ sb_dqn_trade.py
@@ -1,14 +1,9 @@
-'''
-This file is a copy of deprecated version of /sb_recurrent_ppo_trade.py
-This version contains balance which is not used in new version
-'''
-from stable_baselines3 import PPO, A2C
-from sb3_contrib import RecurrentPPO
+from stable_baselines3 import DQN
 import datetime
 
 from environment.trade import Env, TensorboardCallback
 
-MODE = 1 # 0: train, 1: test
+MODE = 0  # 0: train, 1: test
 
 if __name__ == '__main__':
 
@@ -18,11 +13,11 @@ if __name__ == '__main__':
         start = datetime.datetime.now()
         print(f'start: {start}')
 
-
-        model = RecurrentPPO('MlpLstmPolicy', env, learning_rate=3e-4, verbose=1, tensorboard_log='./ppo_trade') # , n_steps=1205
+        model = DQN('MlpPolicy', env, learning_rate=3e-4, verbose=1,
+                             tensorboard_log='./tensorboard/dqn_trade')  # , n_steps=1205
         model.learn(total_timesteps=1_000_000, progress_bar=True, callback=TensorboardCallback())
 
-        model.save('trade_recurrent_ppo_2')
+        model.save('model/trade_dqn_1')
 
         end = datetime.datetime.now()
 
@@ -33,31 +28,32 @@ if __name__ == '__main__':
     elif MODE == 1:
         from torch.utils.tensorboard import SummaryWriter
         import os
+
         env = Env(train=False)
 
-        model = RecurrentPPO.load('trade_recurrent_ppo_2')
-        
-        tb_dir = 'ppo_trade2_evaluate'
+        model = DQN.load('model/trade_dqn_1')
+
+        tb_dir = 'tensorboard/dqn_trade1_evaluate'
         # if not os.path.isdir(tb_dir):
         #     os.mkdir(tb_dir)
 
         obs, info = env.reset()
-        
+
         writer = SummaryWriter(os.path.join(tb_dir, info['stock_id']))
-        
+
         step_count = 0
         while True:
             action, _states = model.predict(obs, deterministic=True)
             obs, rewards, terminated, truncated, info = env.step(action)
 
             writer.add_scalar('env/hold', info['hold'], step_count)
-            writer.add_scalar('env/balance', info['balance'], step_count)
+            # writer.add_scalar('env/balance', info['balance'], step_count)
             writer.add_scalar('env/holding_count', info['holding_count'], step_count)
             writer.add_scalar('env/net', info['net'], step_count)
-            writer.add_scalar('env/net_not_include_settlement', info['net_not_include_settlement'], step_count)
-            
+            writer.add_scalar('env/net_exclude_settlement', info['net_exclude_settlement'], step_count)
+
             step_count += 1
-            
+
             if terminated:
                 writer.close()
                 if info['finish']:
@@ -65,9 +61,9 @@ if __name__ == '__main__':
                 obs, info = env.reset()
                 writer = SummaryWriter(os.path.join(tb_dir, info['stock_id']))
                 step_count = 0
-                env.balance = 1_000_000 # 重置餘額
-                env.net = 0 # 重置淨損益
-                env.net_not_include_settlement = 0 # 重置淨損益(不含結算)
+                # env.balance = 1_000_000 # 重置餘額
+                env.net = 0  # 重置淨損益
+                env.net_not_include_settlement = 0  # 重置淨損益(不含結算)
 
     else:
         raise ValueError('MODE must be 0 or 1')
---
+++ sb_sac_trade.py
@@ -0,0 +1,1 @@
+# sac only support box action space
