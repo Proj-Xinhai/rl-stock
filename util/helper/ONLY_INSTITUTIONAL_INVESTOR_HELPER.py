7d0b76faa1ed4100614dfcfed392e3f606688e32
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2023-10-17 20:49:09 +0800

    new helper finished

---
+++ util/helper/ONLY_INSTITUTIONAL_INVESTOR_HELPER.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import gymnasium.spaces as spaces
+from sklearn.preprocessing import MinMaxScaler
+from typing import Tuple, Any
+from util.pipeline_helper import BasicPipelineHelper
+
+
+class OnlyInstitutionalInvestorHelper(BasicPipelineHelper):
+    def __init__(self):
+        super(OnlyInstitutionalInvestorHelper, self).__init__()  # 繼承父類別的__init__()
+        self.observation_space = spaces.Box(
+            low=np.array([np.concatenate([np.zeros(1), -np.ones(7), np.array([-np.inf])])]),
+            high=np.array([np.concatenate([np.ones(1), np.ones(7), np.array([np.inf])])]),
+            shape=(1, 1 + 7 + 1),  # 1: 收盤價, 7: 法人買賣超, 1: 持有量
+            dtype=np.float32
+        )
+        self.action_space = spaces.Discrete(3)
+
+    def data_getter(self, stock_num: str, is_train: bool = True) -> pd.DataFrame:
+        data_path = 'data/train' if is_train else 'data/test'  # 資料路徑
+
+        # 個股資料
+        s = pd.read_csv(f'{data_path}/個股/{stock_num}.csv').reset_index(drop=True).set_index('Date')
+        s = s[['Close']]  # 只保留收盤價
+        # 三大法人買賣超
+        i = pd.read_csv(f'{data_path}/法人買賣超日報_個股/{stock_num}.csv', index_col=0).reset_index(
+            drop=True).set_index('Date')
+
+        # 正規化法人買賣超，且只保留買賣超部分
+        i = i.apply(lambda x: x.apply(lambda y: y.replace(',', '') if type(y) == str else y))
+        i = i[['外陸資買賣超股數(不含外資自營商)', '外資自營商買賣超股數', '投信買賣超股數', '自營商買賣超股數',
+               '自營商買賣超股數(自行買賣)', '自營商買賣超股數(避險)', '三大法人買賣超股數']]
+        i = i.apply(lambda x: x.apply(lambda y: 1 if float(y) > 0 else -1 if float(y) < 0 else 0))
+        i = i.shift(1)  # 往下偏移一天 (因為當天結束才會統計買賣超資訊，實務上交易日當天是不知道當天買賣超資訊的)
+        i = i.fillna(0)  # 用0補空值 (影響應該不會太大)
+
+        # 合併資料，並把含有空值之列刪除
+        # 空值原因: 部分補班日不開盤，但是含有法人買賣超資料，此時個股資料會有空值
+        data = pd.concat([s, i], axis=1, sort=True).dropna()
+
+        return data
+
+    def data_preprocess(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, Any]:
+        scaler = MinMaxScaler()
+        scaler.fit(data['Close'].values.reshape(-1, 1))
+        # 收盤價
+        data['Close'] = scaler.transform(data['Close'].values.reshape(-1, 1)).reshape(-1)
+
+        return data, scaler
+
+    def action_decoder(self, action: Any) -> Any:
+        return bool(action) if action != 2 else None
+
+
+EXPORT = OnlyInstitutionalInvestorHelper
+DESCRIPT = 'institutional investor data only (normal_helper)'
---
+++ util/helper/ONLY_MACD_HELPER.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import gymnasium.spaces as spaces
+import talib
+from sklearn.preprocessing import MinMaxScaler
+from typing import Tuple, Any
+from util.pipeline_helper import BasicPipelineHelper
+
+
+class OnlyMACDHelper(BasicPipelineHelper):
+    def __init__(self):
+        super(OnlyMACDHelper, self).__init__()  # 繼承父類別的__init__()
+        self.observation_space = spaces.Box(
+            low=np.array([np.concatenate([np.zeros(1), np.array([-np.inf] * 3), np.array([-np.inf])])]),
+            high=np.array([np.concatenate([np.ones(1), np.array([np.inf] * 3), np.array([np.inf])])]),
+            shape=(1, 1 + 3 + 1),  # 1 收盤價, 3: MACD, 1: 持有量
+            dtype=np.float32
+        )
+        self.action_space = spaces.Discrete(3)
+
+    def data_getter(self, stock_num: str, is_train: bool = True) -> pd.DataFrame:
+        data_path = 'data/train' if is_train else 'data/test'  # 資料路徑
+
+        # 個股資料
+        s = pd.read_csv(f'{data_path}/個股/{stock_num}.csv').reset_index(drop=True).set_index('Date')
+        s = s[['Close']]  # 只保留收盤價
+
+        # 合併資料，並把含有空值之列刪除
+        # 空值原因: 部分補班日不開盤，但是含有法人買賣超資料，此時個股資料會有空值
+        data = pd.concat([s], axis=1, sort=True).dropna()
+
+        # 先合併完，再算技術指標 (避免dropna把開頭的資料刪掉)
+        # MACD
+        data['MACD'], data['MACDsignal'], data['MACDhist'] = (
+            talib.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9))
+
+        """
+        補空值
+        作法: 直接補0
+        """
+        data = data.fillna(0)
+
+        return data
+
+    def data_preprocess(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, Any]:
+        scaler = MinMaxScaler()
+        scaler.fit(data['Close'].values.reshape(-1, 1))
+        # 收盤價
+        data['Close'] = scaler.transform(data['Close'].values.reshape(-1, 1)).reshape(-1)
+
+        return data, scaler
+
+    def action_decoder(self, action: Any) -> Any:
+        return bool(action) if action != 2 else None
+
+
+EXPORT = OnlyMACDHelper
+DESCRIPT = 'macd data only'
