ef994d556b949a814961b51a2d6811bacc9e56cb
sakkyoi <22865542+sakkyoi@users.noreply.github.comtop>
2023-10-18 09:45:51 +0800

    update for dev branch

--- api/util/get_git_hash.py
+++ api/util/get_git_hash.py
@@ -1,12 +1,13 @@
 import git
+from typing import Tuple, Optional
 
 
-def get_git_hash():
+def get_git_hash() -> Tuple[Optional[str], Optional[str]]:
     try:
         repo = git.Repo(search_parent_directories=True)
-        return repo.head.object.hexsha
+        return repo.active_branch.object.name, repo.active_branch.commit.hexsha
     except git.exc.InvalidGitRepositoryError:
-        return None
+        return None, None
 
 
 if __name__ == '__main__':
